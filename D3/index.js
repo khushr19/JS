6d576646050cd447a28e65b3c89fa84e5700847b
khushr19 <bindkhushi36@gmail.com>
2025-06-28 13:12:53 +0530

    upd

---
+++ D3/index.js
@@ -0,0 +1,27 @@
+//! Conditionals in JavaScript
+
+let age = 18;
+
+if (age > 18) {
+    console.log("You are not eligible to vote");
+} else {
+    console.log("You are eligible to vote");
+}
+
+//! Switch in JavaScript
+
+let weather = "monsoon";
+
+switch (weather) {
+    case "monsoon":
+        console.log("You selected Monsoon");
+        break;
+    case "winter":
+        console.log("You selected Winter");
+        break;
+    case "summer":
+        console.log("You selected Summer");
+        break;
+    default:
+        console.log("You selected none");
+}
