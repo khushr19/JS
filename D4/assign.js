696151553aa13475ffeb67f771e4453ba6d5c402
khushr19 <bindkhushi36@gmail.com>
2025-06-29 11:28:38 +0530

    upd

---
+++ D4/assign.js
@@ -0,0 +1,79 @@
+// 
+//for
+// for(var i=1;i<=50;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+// 
+// var i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+// 
+// let i=1;
+// for(i;i<=10;i++){
+//     console.log("7*",i,"=",7*i)
+// }
+// 
+// for(i=1;i<=100;i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+// count=0;
+// for(var i=1;i<=100;i++){
+//     if(i%3==0 && i%5==0){
+//         // console.log(i);
+//         count++;
+//     }
+// }
+// console.log(count);
+// let fact=1;
+// let n=7;
+// for(var i=1;i<=7;i++){
+//     fact*=i;
+// }
+// console.log(fact);
+// 
+// let i=1;
+// count=1;
+// for(var n=1;n<=100;n++){
+//     count+=n;
+// }
+// console.log(count);
+// let a=15;
+// // let fixb;
+// switch(a){
+//     case a%3==0:
+//         console.log("Fizz");
+//     break;
+//     case a%5==0:
+//         console.log("Buzz");
+//     break;
+//     case a%3==0 && a%5==0 :
+//         console.log("FizzBuzz");
+//     break;
+//     default:
+//         console.log("default");
+// }
+// for(var i=1;i<=50;i++){
+//     console.log(i);
+// }
+for(var a=1;a<=10;a++){
+    a=a*a;
+}
+// console.log(a);
+// if(a<20){
+//     console.log("small");
+// }
+// else if(a>=20 && a<=50){
+//     console.log("medium");
+// }
+// else if(a>50){
+//     console.log("large");
+// }
+// else{
+//     console.log("default");
+// }
