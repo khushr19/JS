782d73e6fcae4de48f6b70d900f3f6ea48e263a2
khushr19 <bindkhushi36@gmail.com>
2025-07-19 14:17:17 +0530

    upd

---
+++ test3.js/t3.js
@@ -0,0 +1,70 @@
+//Q9
+let arr=[1,3,5,7,9];
+target=5;
+for(let i=0;i<arr.length;i++){
+    if(target===arr[i]){
+        console.log(arr[i]);
+    }
+}
+//Q6
+let arr2=[1,2,3,5,6];
+target2=4;
+while(left<right){
+left=0;
+right=arr.length-1;//4
+midindex=Math.round((left+right)/2);//2
+midelement=midindex[i];//3
+if(midelement<rightelement){
+    console.log("Missing number");
+}
+}
+//Q1
+let arr3=[4,5,1,2,0,4,1,0];
+
+let count=0;
+for(let i=0;i<arr3.length;i++){
+    if(arr3[i]!==arr3[i]){
+        console.log(arr[i]);
+    }
+}
+// console.log();
+
+
+//Q3
+let a1=[1,2,3];
+let a2=[2,3,4];
+let a3=a1.concat(a2);
+console.log(a3);
+for(let i=0;i<a3.length;i++){
+if(a3[i]==a3[i]){
+    a3.pop(arr[i]);
+}
+}
+console.log(a3[i]);
+
+//Q6
+let a4=[1,2,4,7];
+let t=6;
+for(let i=0;i<a4.length;i++){
+    if((arr[i]+arr[i])===t){
+        console.log("true");
+    }
+}
+
+//Q7
+let obj={a:1,b:2};
+let array=[];
+Object.entries(obj=>array[]);
+console.log(array[]);
+
+
+//Q10
+let arry2=[1,1,2,3,3,3];
+let count=0;
+for(let i=0;i<arry2.length;i++){
+    if(arry2[i]===arry2[i]){
+        count++;
+    }
+}
+console.log(arry2);
+
